cf649fec19a7d68c9ca6679402a08a1c3632a49d
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2018-02-15 21:21:40 -0800

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+# Twitter Lite
+
+A lightweight, fully-featured, client & server library for the Twitter API
+
+- Promise-first via Async / Await
+- Up-to-date
+- Under 1kb
+
+## Installation
+
+```
+yarn add twitter-lite
+```
+
+```
+npm install twitter-lite
+```
+
+## Usage
+
+```es6
+const client = new Twitter({
+  subdomain: "api",
+  consumer_key: "xyz" // from Twitter.
+  consumer_secret: "xyz" // from Twitter.
+  access_token_key: "abc" // from your User (oauth_token)
+  access_token_secret: "abc" // from your User (oauth_token_secret)
+})
+
+client.get("account/verify_credentials").then(results => {
+  console.log("results", results)
+}).catch(console.error)
+```
+
+## Streams
+
+TBD
+
+I don't know shit about streams. If you can help make this work with streams, I would appreciate & encourage it!
+
+## Credit
+
+Over the years, thanks to:
+- [@technoweenie](http://github.com/technoweenie)
+- [@jdub](http://github.com/jdub)
+- [@desmondmorris](http://github.com/desmondmorris)
+- [Node Twitter Community](https://github.com/desmondmorris/node-twitter/graphs/contributors)
---
+++ twitter.js
@@ -0,0 +1,79 @@
+const crypto = require("crypto");
+const OAuth = require("oauth-1.0a");
+const SubIn = require("sub-in");
+const Fetch = require("node-fetch");
+
+const getUrl = subdomain => SubIn("https://$0.twitter.com/1.1", [subdomain]);
+const createOauthClient = ({ key, secret }) => {
+  const client = OAuth({
+    consumer: { key, secret },
+    signature_method: "HMAC-SHA1",
+    hash_function(baseString, key) {
+      return crypto
+        .createHmac("sha1", key)
+        .update(baseString)
+        .digest("base64");
+    }
+  });
+
+  return client;
+};
+
+const defaults = {
+  subdomain: "api",
+  consumer_key: null,
+  consumer_secret: null,
+  access_token_key: null,
+  access_token_secret: null
+};
+
+class Twitter {
+  constructor(options) {
+    const config = Object.assign({}, defaults, options);
+    this.client = createOauthClient({
+      key: config.consumer_key,
+      secret: config.consumer_secret
+    });
+
+    this.token = {
+      key: config.access_token_key,
+      secret: config.access_token_secret
+    };
+
+    this.url = getUrl(config.subdomain);
+  }
+
+  async get(resource) {
+    const requestData = {
+      url: `${this.url}/${resource}.json`,
+      method: "GET"
+    };
+
+    const headers = this.client.toHeader(
+      this.client.authorize(requestData, this.token)
+    );
+
+    const results = await Fetch(requestData.url, { headers }).then(res =>
+      res.json()
+    );
+    return results;
+  }
+
+  async post(resource) {
+    const requestData = {
+      url: `${this.url}/${resource}.json`,
+      method: "POST"
+    };
+    const headers = this.client.toHeader(
+      this.client.authorize(requestData, this.token)
+    );
+
+    const results = await Fetch(requestData.url, {
+      method: "POST",
+      headers
+    }).then(res => res.json());
+    return results;
+  }
+}
+
+module.exports = Twitter;
