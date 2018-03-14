a3986aa0617c973ef371864c819fa271185e96b8
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2018-03-13 20:53:37 -0700

    prettier fix

--- twitter.js
+++ twitter.js
@@ -57,8 +57,7 @@ class Twitter {
       url: `${this.url}/${resource}.json`,
       method: "GET"
     };
-    if (parameters)
-      requestData.url += "?" + querystring.stringify(parameters);
+    if (parameters) requestData.url += "?" + querystring.stringify(parameters);
 
     let headers = {};
     if (this.authType === "User") {
@@ -83,9 +82,7 @@ class Twitter {
       method: "POST"
     };
 
-    if (parameters) {
-      requestData.url += "?" + querystring.stringify(parameters);
-    }
+    if (parameters) requestData.url += "?" + querystring.stringify(parameters);
 
     let headers = {};
     if (this.authType === "User") {
@@ -114,10 +111,9 @@ class Twitter {
 
     const requestData = {
       url: `${getUrl("stream")}/${resource}.json`,
-      method: "GET",
+      method: "GET"
     };
-    if (parameters)
-      requestData.url += "?" + querystring.stringify(parameters);
+    if (parameters) requestData.url += "?" + querystring.stringify(parameters);
 
     const headers = this.client.toHeader(
       this.client.authorize(requestData, this.token)
