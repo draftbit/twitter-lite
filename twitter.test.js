2130ac712715d9897d94061d0318347aa76fc121
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2018-03-14 17:11:14 -0700

    add favorited tweet test

--- twitter.test.js
+++ twitter.test.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const Twitter = require('./twitter')
 
 const {
@@ -75,4 +76,35 @@ it('should verify credentials with correct tokens', async () => {
     screen_name: 'nodejs_lite',
     description: 'Twitter Lite Testing Account'
   })
+})
+
+it('should show 2 favorited tweets', async () => {
+  const client = new Twitter({
+    subdomain: 'api',
+    consumer_key: TWITTER_CONSUMER_KEY,
+    consumer_secret: TWITTER_CONSUMER_SECRET,
+    access_token_key: ACCESS_TOKEN,
+    access_token_secret: ACCESS_TOKEN_SECRET
+  })
+
+  const response = await client.get('favorites/list')
+  const [first, second] = response
+
+  const results = [
+    {
+      id: first.id,
+    },
+    {
+      id: second.id,
+    }
+  ]
+
+  expect(results).toEqual([
+    {
+      id: 973775515453722600
+    },
+    {
+      id: 972868365898334200
+    }
+  ])
 })
