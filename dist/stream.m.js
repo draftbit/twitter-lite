fc1b9be4a20c39dcb20ce1d512bc5f690060d4b4
Drew Bredvick <dbredvick@gmail.com>
2021-06-13 21:24:08 -0500

    fix: readd build assets until PR accepted

---
+++ dist/stream.m.js
@@ -0,0 +1,2 @@
+var t=require("events");module.exports=function(t){function e(){t.call(this),this.buffer=""}return t&&(e.__proto__=t),(e.prototype=Object.create(t&&t.prototype)).constructor=e,e.prototype.parse=function(t){var e,r;for(this.buffer+=t.toString("utf8");(e=this.buffer.indexOf("\r\n"))>-1;)if(r=this.buffer.slice(0,e),this.buffer=this.buffer.slice(e+2),r.length>0)try{r=JSON.parse(r),this.emit(r.event||"data",r)}catch(t){t.source=r,this.emit("error",t)}else this.emit("ping")},e}(t);
+//# sourceMappingURL=stream.m.js.map
